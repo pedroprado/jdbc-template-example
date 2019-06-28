00066ac87a28bf54e4c3f545ab67f8e58151ddff
Pedro Prado <pedro.henrique@certsys.com.br>
2019-06-28 17:41:52 -0300

    first commit

---
+++ src/test/java/jdbcexample/jdbcexample/JdbcExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package jdbcexample.jdbcexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
